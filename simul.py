b463d337aadd201faf5aacca8706f75bf51c80a9
Thales Magalhaes <thls.mglhs@gmail.com>
2017-12-14 15:43:00 -0200

    Refatoração de 'simul.py'

--- simul.py
+++ simul.py
@@ -3,52 +3,45 @@ from collections import *
 from fila import *
 import random
 
-# Configura parser e coleta argumentos
+# Configura parser de argumentos
 parser = argparse.ArgumentParser()
-parser.add_argument('rho', metavar='RHO', type=float,
-	help='utilização do sistema')
-parser.add_argument('m', metavar='M', type=int, default=1e4,
-	help='tamanho da fase transiente')
-parser.add_argument('k', metavar='K', type=int, default=10,
-	help='número de rodadas')
-args = parser.parse_args()
+parser.add_argument('RHO', type=float, help='utilização do sistema')
+parser.add_argument('M', type=int, help='tamanho da fase transiente')
+parser.add_argument('N', type=int, help='tamanho das rodadas incial')
+parser.add_argument('K', type=int, help='número de rodadas')
 
-# Configura seed do gerador
-random.seed(0)
-
-# Encontra o tamanho mínimo da rodada para o intervalo de confiança desejado
-for expn in range(2, 32):
-	n = 1 << expn
-
+def main(rho, m, n, k):
+	"""Chamado recursivamente até encontrar o intervalo desejado"""
 	stats = defaultdict(Sample) # Dicionário contendo estatísticas
-	queue = Queue(args.rho/2)   # Sistema de filas
-	queue.simround(args.m)      # Simula fase transiente
-	for i in range(args.k):
+	queue = Queue(rho/2)        # Sistema de filas
+	queue.simround(m)           # Simula fase transiente
+	for i in range(k):
 		print('N {}: round {} '.format(n, i+1), end='\r')
-		smps, smpfs = queue.simround(n) # Simula rodada coletando estatísticas
+		smps, smpfs = queue.simround(n) # Simula rodada
 
 		# Adiciona estatísticas ao dicionário
 		for name, sample in smps.items() | smpfs.items():
-			name = 'E[' + name + ']'
+			name = 'E[%s]' % name
 			stats[name].append(sample.mean())
 		for name, sample in smps.items():
-			name = 'V(' + name + ')'
+			name = 'V(%s)' % name
 			stats[name].append(sample.var())
 
-	rmargin = []
 	for stat in stats.values():
 		low, high = stat.mean_interval(0.05)
-		rmargin.append((high - low) / stat.mean())
+		rmargin = (high - low) / stat.mean()
+		if rmargin > 0.05:
+			return main(rho, m, 2*n, k)
 
-	if max(rmargin) < 0.05:
-		print('N {}          '.format(n))
-		break
+	# Imprime as estatísticas encontradas e intervalos de confiança
+	print('N {}          '.format(n))
+	for name, stat in sorted(stats.items()):
+		mean = stat.mean()
+		low, high = stat.mean_interval(0.05)
+		rmargin = (high - low) / mean
+		print('{:>6} ={:8.3f} {:5.2f}%'.format(name, mean, 100*rmargin))
 
-# Imprime a média das estatísticas coletadas e seus intervalos de confiança
-for name, stat in sorted(stats.items()):
-	mean = stat.mean()
-	low, high = stat.mean_interval(0.05)
-	rmargin = (high - low) / mean
-	
-	print('{:>6} ={:8.3f} {:5.2f}%'.format(
-		name, mean, 100*rmargin))
+if __name__ == '__main__':
+	random.seed(0)
+	args = parser.parse_args()
+	main(args.RHO, args.M, args.N, args.K)
